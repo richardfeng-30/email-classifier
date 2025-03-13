771e134e90e70993b66b89edf59e0a198a1b27ac
richardfeng-30 <richardqfeng@gmail.com>
2025-03-13 08:57:04 -0700

    improve learning rate and normalize scores

--- email_classifier2.py
+++ email_classifier2.py
@@ -35,48 +35,47 @@ for i in range(len(train_x)):
     if train_y[i] == 1:
         for word in wordList:
             if word in dictionary.keys():
-                dictionary[word] += 1
+                dictionary[word] += 1 / len(wordList)
             else:
-                dictionary[word] = 1
+                dictionary[word] = 1 / len(wordList)
     else:
         for word in wordList:
             if word in dictionary.keys():
-                dictionary[word] -= 1
+                dictionary[word] -= 1 / len(wordList)
             else:
-                dictionary[word] = -1
+                dictionary[word] = -1 / len(wordList)
 
 # train accuracy
 print("train accuracy:")
-last_accuracy = -1
 accuracy = 0
+last_accuracy = -1
 i = 0
-while accuracy - last_accuracy > 0.001 or accuracy < 0.99:
+learning_rate = 20
+while abs(accuracy - last_accuracy) > 0.0005:
     correct = 0
+    last_accuracy = accuracy
     for j in range(len(train_x)):
         wordList = train_x[j].split()
-
         score = 0
         for word in wordList:
             if word in dictionary.keys():
                 score += dictionary[word]
-
         if score >= 0:
             if train_y[j] == 1:
                 correct += 1
             else:
                 for word in wordList:
-                    dictionary[word] -= 1
+                    dictionary[word] -= learning_rate / len(wordList)
         else:
             if train_y[j] == 0:
                 correct += 1
             else:
                 for word in wordList:
-                    dictionary[word] += 1
-    last_accuracy = accuracy
+                    dictionary[word] += learning_rate / len(wordList)
     accuracy = correct / len(train_x)
-    print(i + 1, "-", accuracy)
+    print("pass", i + 1, "- accuracy", accuracy)
     i += 1
-
+    learning_rate = 20 * (0.9 ** i)
 
 # validation
 correct = 0
